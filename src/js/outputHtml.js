3a0ca2c4ee23145531114729362b14fef26ef36c
Kyle Denver Morena <kyledenvermorena@gmail.com>
2021-02-26 14:32:19 +0100

    Ready to firebase

--- src/js/outputHtml.js
+++ src/js/outputHtml.js
@@ -25,7 +25,7 @@ const outputHtml = {
         if(res.aqi=='-'){ res.aqi = '';}
         return res = `
         <a href="#" class="col p-3 no-decoration" data-latlng="${res.lat};${res.lon}" >
-          <div class="d-flex flex-column justify-content-center text-center bg-secondary">
+          <div class="d-flex flex-column justify-content-center text-center">
             <h4 class="p-2 mb-0">${res.station.name}</h4>
             <p class="lead mb-0">
               <strong style="color:${res.aqiDescription.color}">${res.aqi}</strong> 
@@ -57,16 +57,16 @@ const outputHtml = {
       if(match.aqiDescription.value!='Not available'){
         if(match.iaqi.h===undefined){match.iaqi['h'] = {'v': "no data"}}
         cardContainer.innerHTML = `
-        <div class="card border-0 text-center bg-secondary card-height">
-          <div class="card-header bg-transparent border-dark">
+        <div class="card rounded-sm border-0 text-center card-height">
+          <div class="card-header bg-transparent border-light">
             <h2>${match.city.name}</h2>
           </div>
           <div class="card-body d-flex align-items-center justify-content-center flex-column">
-            <h3 class="card-title">
+            <h3 class="card-title p-2 rounded-sm bg-dark">
               <span style="color:${match.aqiDescription.color}">${match.aqi}</span> 
               <span style="color:${match.aqiDescription.color}">${match.aqiDescription.level}</span>
             </h3>
-            <p class="lead card-text mb-0">${match.aqiDescription.health}</p>
+            <p class="lead card-text mb-0 p-1">${match.aqiDescription.health}</p>
             <ul class="list-group list-group-flush mt-3 rounded-lg">
               <li class="list-group-item bg-transparent">Temp: ${match.iaqi.t.v}°C</li>
               <li class="list-group-item bg-transparent">Pressure: ${match.iaqi.p.v}</li>
@@ -74,7 +74,7 @@ const outputHtml = {
               <li class="list-group-item bg-transparent">Wind: ${match.iaqi.w.v} km/h</li>
             </ul>
           </div>
-          <div class="card-footer bg-transparent border-dark">${date}</div>
+          <div class="card-footer bg-transparent border-light">${date}</div>
         </div>`
       }else{
         cardContainer.innerHTML = `
@@ -83,7 +83,7 @@ const outputHtml = {
             <h2>${match.city.name}</h2>
           </div>
           <div class="card-body d-flex text-center justify-content-center flex-column">
-            <h3 class="card-title">
+            <h3 class="card-title p-2 rounded bg-dark">
               <span style="color:${match.aqiDescription.color}">${match.aqi}</span> 
               <span style="color:${match.aqiDescription.color}">${match.aqiDescription.level}</span>
             </h3>
